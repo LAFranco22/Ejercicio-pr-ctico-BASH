c7734ad86b0ffc5dae006be23bc26c562ea16777
MilagrosBorgstrand <mborgstrand@fi.uba.ar>
2026-03-25 13:50:36 -0300

    Enhance consolidar.sh with file checks
    Added checks for non-empty FILENAME variable and file existence.

--- consolidar.sh
+++ consolidar.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
+## Si la variable no está vacía... recorre archivos txt
 if  [ -n "$FILENAME" ];then
 
      for archivo in "$HOME/EPNro1/entrada/"*.txt
      do
 
+## Si existe archivo o carpeta, los junta y los mueve a los archivos ya procesados.
          if [ -e "$archivo" ];then
 
              cat "$archivo" >> "$HOME/EPNro1/salida/$FILENAME.txt"
